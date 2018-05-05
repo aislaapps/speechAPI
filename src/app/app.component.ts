2c4a9ac7322f9395710100bd95784e8b3fb71024
aisla <aislaapps@gmail.com>
2018-05-05 22:03:41 +1000

    Speech Recognition API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SpeechRecognitionService } from './speech-recognition.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  showSearchButton: boolean;
+  stopListeningButton: boolean;
+  speechData: string;
+
   title = 'app';
+
+  constructor(private speechRecognitionService: SpeechRecognitionService) {
+    this.showSearchButton = true;
+    this.stopListeningButton = false;
+    this.speechData = "";
+  }
+
+  ngOnInit() {
+    console.log("hello there")
+  }
+
+  ngOnDestroy() {
+    this.speechRecognitionService.DestroySpeechObject();
+  }
+
+
+  activateSpeechSearch(): void {
+    this.showSearchButton = false;
+
+    this.speechRecognitionService.record()
+        .subscribe(
+        //listener
+        (value) => {
+            this.speechData = value;
+            console.log('listener.speechData:', value);
+        },
+        //error
+        (err) => {
+            console.log(err);
+            if (err.error == "no-speech") {
+                console.log("--restarting service--");
+                this.activateSpeechSearch();
+            }
+        },
+        //completion
+        () => {
+            this.showSearchButton = true;
+            console.log("--complete--");
+            if (!this.stopListeningButton) {
+              this.activateSpeechSearch();
+            }
+        });
+  }
+
+  deActivateSpeechSearch(): void {
+    this.showSearchButton = true;
+    this.stopListeningButton = true;
+    this.speechRecognitionService.DestroySpeechObject();
+  }
+
 }
